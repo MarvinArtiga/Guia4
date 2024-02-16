1fe9d7273fcf5c10508a90c8788c10293d3d75b8
Alexis625 <bluciano658@gmail.com>
2024-02-16 08:03:06 -0600

    agregar mas libros (parte de la discusion de ejercicios)

--- src/app/app/data.js
+++ src/app/app/data.js
@@ -26,10 +26,54 @@ export const data=[
     price:30,
     urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
     quantity:1
-    }, {
+    },
+     {
     id:5,
     title:'Harry Potter Pack',
     price:390,
     urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_c   om=1113',quantity:1
-}
+    },
+
+    // aqui van los nuevos 5 libros
+        {
+        id:6,
+        title:'Maze runer',
+        price:15,
+        urlImage:'https://www.zigzag.cl/pub/media/catalog/product/cache/f40365484e36b02d74dc5711a111d955/9/7/9789876122672_1.jpg',
+        quantity:1
+        },
+
+        {
+        id:7,
+        title:'Padre rico padre pobre',
+        price:25,
+        urlImage:'https://panamericana.vtexassets.com/arquivos/ids/272725/padre-rico-padre-pobre-9789585433595.jpg?v=636403058688000000',
+        quantity:1
+        },
+
+        {
+        id:8,
+        title:'El hombre mas rico de babilonia',
+        price:50,
+        urlImage:'https://librerialerner.vteximg.com.br/arquivos/ids/1184187-492-492/principal.jpg?v=638381588417730000',
+        quantity:1
+        },
+
+        {
+        id:9,
+        title:'Los juegos del hambre',
+        price:15,
+        urlImage:'https://images.cdn3.buscalibre.com/fit-in/360x360/4e/1c/4e1c899ff4709b0a9a884362611b66fb.jpg',
+        quantity:1
+        },
+
+        {
+        id:10,
+        title:'Luna de pluton',
+        price:8,
+        urlImage:'https://resources.sanborns.com.mx/imagenes-sanborns-ii/1200/9786070760846_2.jpg?scale=500&qlty=75',
+        quantity:1
+        },
+
+
 ];
